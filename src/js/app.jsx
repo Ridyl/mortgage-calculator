18fdfa5edde5abf144bb3fa217bf5bfeca74432a
Ridyl <joey.dalatri214@gmail.com>
2024-12-31 16:18:38 -0500

    Deleted node_modules and package-lock.json, Installed Compatible Webpack Loaders, Verified Webpack and Webpack-CLI Compatibility, Adjusted Webpack Configuration, Addressed Webpack-CLI Schema Error, Reinstalled All Dependencies

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,12 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 
-export default class App extends React.Component {
-  // your Javascript goes here
-  render() {
-    return (
-      <div className='container'>
-        {/* your JSX goes here */}
-      </div>
-    );
-  }
+export default function App() {
+  const name = 'Joey';
+  return (
+    <div className='container'>
+      <h1 className='title-test'>{name}</h1>
+      <button className='btn btn-primary'>TEST</button>
+    </div>
+  );
 }
