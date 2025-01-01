4fdcd71b06d53d8aa807681de1b43650d481479c
Ridyl <joey.dalatri214@gmail.com>
2024-12-31 19:21:40 -0500

    Finished barebones layout using React and Bootstrap. No calculator functionality at the moment.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,11 +3,28 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 
 export default function App() {
-  const name = 'Joey';
   return (
     <div className='container'>
-      <h1 className='title-test'>{name}</h1>
-      <button className='btn btn-primary'>TEST</button>
+      <h1 className='title-test'>Mortgage Calculator</h1>
+      <form className='form-container'>
+        <div className='row- md'>
+          <label htmlFor='top-input'>Loan Balance</label>
+          <input className='form-control form-control-lg' type='text' id='top-input'/>
+        </div>
+        <div className='row-md'>
+          <label htmlFor='bottom-input'>Interest Rate (%)</label>
+          <input className='form-control form-control-lg' type='text' id='bottom-input' />
+        </div>
+        <div className='row-md'>
+          <label htmlFor='term'>Loan Term</label>
+          <select className='form-control form-control-lg' id='term'>
+            <option>15</option>
+            <option>30</option>
+          </select>
+        </div>
+        <button className='btn btn-primary'>Calculate</button>
+      </form>
+      <div className='display' />
     </div>
   );
 }
