7e8a867dec1ccc4066f33b039eb6b92419d4f54a
Ridyl <joey.dalatri214@gmail.com>
2025-01-02 20:18:57 -0500

    Logic is finished and everything in theory should work, need to create a new project to update react without breaking anything

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,30 +1,118 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default function App() {
-  return (
-    <div className='container'>
-      <h1 className='title-test'>Mortgage Calculator</h1>
-      <form className='form-container'>
-        <div className='row- md'>
-          <label htmlFor='top-input'>Loan Balance</label>
-          <input className='form-control form-control-lg' type='text' id='top-input'/>
+// input creator, will create all labels and drop downs if value is present
+class Output extends Component {
+  render() {
+    const { label, dropDown, name, type, step } = this.props;
+    // set label element and html text
+    const createLabel = (
+      <label htmlFor='user-input' className='col-sm-2 col-form-label fs-5' placeholder={ '0' }>
+        {label}
+      </label>
+    );
+
+    // if dropDown has no value return default input field with label
+    if (!dropDown) {
+      const userInput = (
+        <input
+          className='form-control form-control-lg'
+          name={ name }
+          type={ type }
+          step={ step || undefined }
+        />
+        );
+
+      return (
+        <div className='for-group row'>
+          {createLabel}
+          <div className='col-sm-10'>
+            { userInput }
+          </div>
         </div>
-        <div className='row-md'>
-          <label htmlFor='bottom-input'>Interest Rate (%)</label>
-          <input className='form-control form-control-lg' type='text' id='bottom-input' />
-        </div>
-        <div className='row-md'>
-          <label htmlFor='term'>Loan Term</label>
+      );
+    }
+
+    // creates each drop down option
+    const drop = dropDown.map((option, num) => <option key={ num }>{ option }</option>);
+
+    // if dropDown has value return drop down with label
+    return (
+      <div className='for-group row'>
+        {createLabel}
+        <div className='col-sm-10'>
           <select className='form-control form-control-lg' id='term'>
-            <option>15</option>
-            <option>30</option>
+            {drop}
           </select>
         </div>
-        <button className='btn btn-primary'>Calculate</button>
-      </form>
-      <div className='display' />
-    </div>
-  );
+      </div>
+    );
+  }
 }
+
+Output.propTypes = {
+  label: PropTypes.string.isRequired,
+  dropDown: PropTypes.arrayOf(PropTypes.number),
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  step: PropTypes.number,
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.formRef = React.createRef();
+    this.outputRef = React.createRef();
+    this.calculateMonthly = this.calculateMonthly.bind(this);
+  }
+
+  calculateMonthly(event) {
+    event.preventDefault();
+
+    const form = this.formRef.current;
+    const balance = parseFloat(form.balance.value);
+    const rate = parseFloat(form.rate.value) / 100;
+    const term = form.term.value;
+
+    if (isNaN(balance) || isNaN(rate) || isNaN(term)) {
+      this.outputRef.current.textContent = 'Please provide valid inputs.';
+      return;
+    }
+
+    const months = term * 12;
+    const monthlyInt = rate / 12;
+
+    const top = monthlyInt * Math.pow((1 + monthlyInt), months);
+    const bottom = Math.pow((1 + monthlyInt), months) - 1;
+
+    const monthlyPayment = balance * (top / bottom);
+
+    this.outputRef.current.textContent = `Monthly Patment: $${monthlyPayment.toFixed(2)}`;
+  }
+
+  render() {
+    return (
+      <div>
+        <form
+          className='container mt-5 w-50'
+          ref={ this.formRef }
+          onSubmit={ this.calculateMonthly }
+        >
+          <h1 className='mb-5 align-center'>Mortgage Calculator</h1>
+
+          <Output label={ 'Loan Amount' } name='balance' type='number' />
+          <Output label={ 'Interest Rate (%)' } name='rate' type='number' step={ 0.01 } />
+          <Output label={ 'Loan Term (years)' } name='term' dropDown={ [15, 30] } />
+
+          <button className='btn btn-primary btn-lg' name='submit'>Calculate</button>
+        </form>
+
+        <div className='display mt-4 text-center' id='output' ref={ this.outputRef } />
+      </div>
+    );
+  }
+}
+
+export default App;
